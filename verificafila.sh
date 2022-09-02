d693eb4647ed8ee0d6d0876c44700d4279e9e204
rafaelwdornelas <rafaelwdornelas@gmail.com>
2022-09-02 15:20:03 -0300

    Update verificafila.sh

--- verificafila.sh
+++ verificafila.sh
@@ -1,27 +1,7 @@
 #!/bin/sh
-
-
-
 while true;do
 clear
-qdir=`/usr/sbin/postconf -h queue_directory` 
-
-incoming=`/usr/bin/find $qdir/incoming -type f -print | wc -l | awk '{print $1}'`
-maildrop=`/usr/bin/find $qdir/maildrop -type f -print | wc -l | awk '{print $1}'`
-active=`/usr/bin/find $qdir/active -type f -print | wc -l | awk '{print $1}'`
-deferred=`/usr/bin/find $qdir/deferred -type f -print | wc -l | awk '{print $1}'`
-hold=`/usr/bin/find $qdir/hold -type f -print | wc -l | awk '{print $1}'`
-corrupt=`/usr/bin/find $qdir/corrupt -type f -print | wc -l | awk '{print $1}'`
-
-# imprimindo informacoes
- 
-echo "Contador de filas Postfix"
-echo -e "\nE-mails chegando (incoming queue): $incoming"
-echo "E-mails sendo processados (maildrop): $maildrop"
-echo "E-mails na fila para entrega (active queue): $active"
-echo "E-mails que nao foram entregues (deferred queue): $deferred"
-echo -e "E-mails com problemas (corrupt queue): $corrupt\n"
-
+cat /var/log/mail.log | ./pflogsumm.pl | more
 sleep 1
 done
 
