09652641682b2a1a24963480a74858f85cec03de
rafaelwdornelas <rafaelwdornelas@gmail.com>
2022-09-05 13:50:44 -0300

    Update dns.js

--- dns.js
+++ dns.js
@@ -5,7 +5,7 @@ const hostName = os.hostname();
 
 const KeyApi = "Qc76OXcZpe99-mIurz68Vfbz503zFOLG0QrrrTKL";
 const Email = "luizfcunhaalves@gmail.com";
-var zone_identifier = "39003307512c9be496cdd9547a301a6b";
+var zone_identifier = "a769f2c1ec9db8df3420a4ac804dbaaa";
 var dkim;
 (async function () {
   console.log(hostName);
