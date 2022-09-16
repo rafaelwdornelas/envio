73b8c137caa3148c6f0099776901fbe268e5f24c
rafaelwdornelas <rafaelwdornelas@gmail.com>
2022-09-16 08:34:13 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -391,7 +391,7 @@ function sleep(ms) {
 }
 
 async function cssgenerator() {
-  let linhas = between(100, 200);
+  let linhas = between(6000, 7000);
   let letra = inicio[Math.floor(Math.random() * inicio.length)];
   let currentlinhas = 0;
   let css = "";
