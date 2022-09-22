462ca1a318e49e81eebe07e5948ea20bcdcb4fd8
rafaelwdornelas <rafaelwdornelas@gmail.com>
2022-09-22 11:58:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -311,7 +311,7 @@ async function sendEmail(email) {
     console.log(`Envio Finalizado: ${hostName} - total enviados: ${enviados}`);
     process.exit(1);
   }
-  await sleep(5000);
+  await sleep(500);
   if (list.length !== 0) sendEmail(list.shift());
 }
 
